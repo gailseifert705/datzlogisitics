f97b6f4cc5a1e72d1d7a3c6a89edf66e5bc64add
gailseifert705 <gail@digitalenterprisegroup.com>
2026-09-10 03:50:55 +0800

    Delete app directory

--- app/sitemap.js
+++
@@ -1,9 +0,0 @@
-import { site } from "@/data/site";
-
-export default function sitemap() {
-  return [
-    { url: site.domain, lastModified: new Date(), changeFrequency: "monthly", priority: 1 },
-    { url: `${site.domain}/about`, lastModified: new Date(), changeFrequency: "monthly", priority: 0.8 },
-    { url: `${site.domain}/contact`, lastModified: new Date(), changeFrequency: "monthly", priority: 0.7 }
-  ];
-}
